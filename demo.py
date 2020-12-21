b1bf16a27944ea257f731ef679994773c757c34d
NathanaelGastelum <nathanaelgastelum@gmail.com>
2020-12-20 23:59:07 -0800

    changed contains() to set.intersection()

--- demo.py
+++ demo.py
@@ -1,14 +1,23 @@
 # Recipe List
 recipes = {
-    'Old Fashioned': ['whiskey', 'simple', 'bitters']
+    'Old Fashioned': {'whiskey', 'simple', 'bitters'},
+    'Gin Sour': {'gin', 'lemon juice', 'simple'},
+    'Whiskey Sour': {'whiskey', 'lemon juice', 'simple'},
+    'Hot Toddy': {'whiskey', 'lemon juice', 'honey', 'hot water', 'cinnamon stick'},
+    'Sidecar': {'cognac', 'orange liqueur', 'lemon juice'}
 }
 
+input = {'whiskey', 'lemon juice'}
+
 # TODO filter function
-ingredient = 'whiskey'
+result = []
 for (key, value) in recipes.items():
-    if value.__contains__(ingredient):
-        print(key)
+    if value.intersection(input):
+        result.append(key)
+
+print(result)
 
 # Stream(?) filter
 # Sort stream
 # Get full recipe text for selected recipe
+# Dictionary of ingrediencts so one ingredient can add multiple things (ex: lemons would also add lemon juice, lemon twist, lemon slice)
