bfac67ecb7d7c99a86d43c1a2f70f99e5aaacba5
NathanaelGastelum <nathanaelgastelum@gmail.com>
2021-01-07 14:31:34 -0800

    implemented strict/loose filtering option

--- demo.py
+++ demo.py
@@ -15,33 +15,40 @@ def search(input, recipes):
 
 # filter function
 # add recipes that are subsets of input
-# TODO add recipes with intersections
-# filter amount of non intersections allowed
+# add recipes with intersections
+# TODO adjust amount of non intersections allowed
 # sort by number of intersections
-def filter(input, recipes):
+def Filter(input, recipes, strict):
     result = set()
     for (key, recipe) in recipes.items():
         if input.issuperset(recipe):
             result.add(key)
+        if strict == False:
+            for (key, recipe) in recipes.items():
+                if len(recipe) - len(input.intersection(recipe)) <= 1:
+                    result.add(key)
     return result
 
 # Recipe List
 recipes = {
     'Old Fashioned': {'whiskey', 'simple', 'bitters'},
     'Gin Sour': {'gin', 'lemon juice', 'simple'},
-    'Whiskey Sour': {'whiskey', 'lemon juice', 'simple'},
     'Hot Toddy': {'whiskey', 'lemon juice', 'honey', 'hot water', 'cinnamon stick'},
+    'Whiskey Sour': {'whiskey', 'lemon juice', 'simple'},
     'Sidecar': {'cognac', 'orange liqueur', 'lemon juice'}
 }
 
 # Test input
 input = {'whiskey', 'lemon juice'}
-
 print(search(input, recipes))
+
 input = {'whiskey', 'gin', 'lemon juice', 'simple'}
-print(filter(input, recipes))
+print(Filter(input, recipes, False))
 
 # Stream(?) filter
 # Sort stream
 # Get full recipe text for selected recipe
-# Dictionary of ingrediencts so one ingredient can add multiple things (ex: lemons would also add lemon juice, lemon twist, lemon slice)
+# Dictionary of ingrediencts so one ingredient can add multiple things 
+# (ex: lemons would also add lemon juice, lemon twist, lemon slice)
+#   Alternatively, the recipe data will just be "lemon" regardless of what the 
+#   full recipe text is
