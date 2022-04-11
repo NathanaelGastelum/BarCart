43605d0df44c078cb036987e7cd246c7db1b1c56
Nathanael Gastelum <nathanaelgastelum@gmail.com>
2022-04-10 18:45:03 -0700

    Started implementing recipeList

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,19 @@
-import './App.css';
+import React from "react";
+
+// We use Route in order to define the different routes of our application
+import { Route, Routes } from "react-router-dom";
 
 // We import all the components we need in our app
 import Navbar from "./components/navbar";
+import RecipeList from "./components/recipeList";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Routes>
+       <Route exact path="/" element={<RecipeList />} />
+     </Routes>
     </div>
   );
 }
